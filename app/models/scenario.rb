0dd3a76dc98f94315816589a75be8fd87bf1cc33
Mike Tria <mike.tria@backops.co>
2015-01-26 18:49:09 -0800

    Testing around scenarios

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -5,6 +5,8 @@ class Scenario < ActiveRecord::Base
   has_many :savings_accounts
   has_many :income_accounts
 
+  after_initialize :init_report
+
   # For each month, want:
   #   Gross Income
   #   Interest
@@ -23,11 +25,23 @@ class Scenario < ActiveRecord::Base
   #    - Mortgages
   # 
   def project(month)
+    run_lifecycle(month)
+    @report[month] = {
+      gross_income: income_accounts.reduce(BigDecimal.new('0')) { |a, e| a + e.gross(month) },
+      interest: savings_accounts.reduce(BigDecimal.new('0')) { |a, e| a + e.interest(month) },
+      savings_balance: savings_accounts.reduce(BigDecimal.new('0')) { |a, e| a + e.ending_balance(month) }
+    }
+  end
+
+  def run_lifecycle(month)
     income_accounts.each { |account| account.project(month) }
-    # TODO expense_accounts.each { |account| account.project(month) }
     savings_accounts.each { |account| account.project(month) }
-    gross = income_accounts.reduce(BigDecimal.new('0')) { |a, e| a + e.gross(month) }
-    interest = savings_accounts.reduce(BigDecimal.new('0')) { |a, e| a + e.gross(month) }
+  end
+
+  private
+
+  def init_report
+    @report = {}
   end
 
 end
--- spec/models/scenario_spec.rb
+++ spec/models/scenario_spec.rb
@@ -7,4 +7,35 @@ describe Scenario, :type => :model do
     it { should validate_uniqueness_of(:name) }
   end
 
+  let(:savings_account) { build(:savings_account) }
+  let(:income_account) { build(:income_account, savings_account: savings_account, starting_month: savings_account.starting_month) }
+  let(:scenario) { build(:scenario) }
+
+  it 'should run income accounts first and then savings accounts' do
+    month = savings_account.starting_month
+    scenario.income_accounts << income_account
+    scenario.savings_accounts << savings_account
+    report = scenario.project(month)
+    expect(report[:gross_income]).to eq(income_account.gross(month))
+    expect(report[:interest]).to eq(savings_account.interest(month))
+    expect(report[:savings_balance]).to eq(savings_account.ending_balance(month))
+  end
+
+  context 'with more than one account' do
+
+    let(:savings_account2) { build(:savings_account, starting_month: savings_account.starting_month) }
+    let(:income_account2) { build(:income_account, savings_account: savings_account2, starting_month: savings_account.starting_month) }
+
+    it 'should run sums of account info' do
+      month = savings_account.starting_month
+      scenario.income_accounts += [income_account, income_account2]
+      scenario.savings_accounts += [savings_account, savings_account2]
+      report = scenario.project(month)
+      expect(report[:gross_income]).to eq(income_account.gross(month) + income_account2.gross(month))
+      expect(report[:interest]).to eq(savings_account.interest(month) + savings_account2.interest(month))
+      expect(report[:savings_balance]).to eq(savings_account.ending_balance(month) + savings_account2.ending_balance(month))
+    end
+
+  end
+
 end
