6069edfaf01df717849a6115aeb6d606c4474884
Mike Tria <mike.tria@backops.co>
2015-02-18 07:53:26 -0800

    100 Tests!

--- app/models/mutual_fund.rb
+++ app/models/mutual_fund.rb
@@ -16,26 +16,54 @@ class MutualFund < ActiveRecord::Base
 
   def project(month)
     return if month < starting_month
+    return if @transactions.has_key?(month)
+
+    @transactions[month] = {
+      taxable_performance: 0, qualified_performance: 0,
+      taxable_dividends: 0,   qualified_dividends: 0,
+      ending_balance: 0
+    }
+    prior_balance = @transactions[month.prior] ? @transactions[month.prior][:ending_balance] : 0
+    stock_bundles.each do |bundle|
+      if bundle.month_bought == month
+        @transactions[month][:ending_balance] += bundle.amount
+      end
+      activity = bundle.stock_activities.find { |a| a.month == month }
+      if activity
+        if activity.month - bundle.month_bought <= 12
+          @transactions[month][:taxable_performance] += activity.performance
+        else
+          @transactions[month][:qualfied_performance] += activity.performance
+        end
+        @transactions[month][:ending_balance] += activity.performance
+      end
+    end
   end
 
   def taxable_performance(month)
+    @transactions[month] ? @transactions[month][:taxable_performance] : 0
   end
 
   def qualified_performance(month)
+    @transactions[month] ? @transactions[month][:qualified_performance] : 0
   end
 
   def taxable_dividends(month)
+    @transactions[month] ? @transactions[month][:taxable_dividends] : 0
   end
 
   def qualified_dividends(month)
+    @transactions[month] ? @transactions[month][:qualified_dividends] : 0
   end
 
   def ending_balance(month)
+    @transactions[month] ? @transactions[month][:ending_balance] : 0
   end
 
   private
 
   def init
+    @transactions = {}
   end
 
 end
--- spec/models/mutual_fund_spec.rb
+++ spec/models/mutual_fund_spec.rb
@@ -8,4 +8,39 @@ describe MutualFund, :type => :model do
     it { should validate_presence_of(:monthly_interest_rate) }
   end
 
+  describe '#project' do
+
+    context 'with no bundles' do
+      let(:account) { build(:mutual_fund) }
+      it 'should return 0' do
+        account.project(account.starting_month)
+        expect(account.taxable_performance(account.starting_month)).to eq(0)
+        expect(account.qualified_performance(account.starting_month)).to eq(0)
+        expect(account.taxable_dividends(account.starting_month)).to eq(0)
+        expect(account.qualified_dividends(account.starting_month)).to eq(0)
+        expect(account.ending_balance(account.starting_month)).to eq(0)
+      end
+    end
+
+    context 'with one bundle' do
+      let(:account) { build(:mutual_fund) }
+      let(:bundle) { build(:stock_bundle, month_bought: account.starting_month) }
+
+      context 'with activity' do
+        let(:activity) { build(:stock_activity, month: bundle.month_bought) }
+        it 'should use historicals' do
+          bundle.stock_activities << activity
+          account.stock_bundles << bundle
+          account.project(account.starting_month)
+          expect(account.taxable_performance(account.starting_month)).to eq(activity.performance)
+          expect(account.qualified_performance(account.starting_month)).to eq(0)
+          expect(account.taxable_dividends(account.starting_month)).to eq(0)
+          expect(account.qualified_dividends(account.starting_month)).to eq(0)
+          expect(account.ending_balance(account.starting_month)).to eq(bundle.amount + activity.performance)
+        end
+      end
+    end
+
+  end
+
 end
